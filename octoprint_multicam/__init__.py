a6e2d62bd53a12392f34b62c4891abdd22271fe1
Michael Morris <mikedmor@gmail.com>
2018-05-10 11:07:58 -0400

    This should be an actual fix for the data migration issue. All settings may be reset in this update. Sorry for the inconvience

--- octoprint_multicam/__init__.py
+++ octoprint_multicam/__init__.py
@@ -18,8 +18,12 @@ class MultiCamPlugin(octoprint.plugin.StartupPlugin,
     def on_after_startup(self):
         self._logger.info("MultiCam Loaded! (more: %s)" % self._settings.get(["multicamStream1"]))
 
+    def get_settings_version(self):
+        return 1
+
     def on_settings_migrate(self, target, current=None):
-        if current is None or current < 1:
+        if current is None or current < self.get_settings_version():
+            self._logger.debug("Settings Migration Needed! Resetting to defaults!")
             # Reset plug settings to defaults.
             self._settings.set(['multicam_profiles'], self.get_settings_defaults()["multicam_profiles"])
 

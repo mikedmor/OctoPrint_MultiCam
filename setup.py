6a218a577184f4d0b1689b0902e596c3c73dfbc9
Morris <mikedmor@gmail.com>
2023-06-04 15:26:38 -0400

    octoprint requirement - TODO update to 1.9.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/mikedmor/OctoPrint_MultiCam"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["OctoPrint>=1.9.0"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
